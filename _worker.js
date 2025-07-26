6ae3a349bcd58b6ce58a2746ed9b8a67ee3dafd5
wesmile8 <75376418+wesmile8@users.noreply.github.com>
2025-07-27 04:10:09 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -17,8 +17,8 @@ https://raw.githubusercontent.com/mfuu/v2ray/master/v2ray
 `;
 
 let urls = [];
-let subConverter = "SUBAPI.cmliussss.net"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
-let subConfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_MultiCountry.ini"; //订阅配置文件
+let subConverter = "hk-subconverter.554994.xyz"; //在线订阅转换后端，目前使用CM的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
+let subConfig = "https://raw.githubusercontent.com/wesmile8/clash/refs/heads/main/YzyCustom.ini"; //订阅配置文件
 let subProtocol = 'https';
 
 export default {
